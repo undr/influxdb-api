6a70841a7a973675a17cb72cd6444f93a2b2e36e
undr <undr@yandex.ru>
2015-03-01 12:37:47 +0700

    Fixed specs for 0.8.8 version

--- spec/intergrations/continuous_queries_spec.rb
+++ spec/intergrations/continuous_queries_spec.rb
@@ -23,7 +23,7 @@ describe 'continuous_queries', integration: true do
       end
     end
 
-    context 'when there are some continuous queries' do
+    context 'when there are some continuous queries', version: '<=0.8.3' do
       let(:continuous_query1){ 'select MEAN(user) as user_mean from cpu group by time(1m) into cpu.1m.user_mean' }
       let(:continuous_query2){ 'select MAX(user) as user_max from cpu group by time(1m) into cpu.1m.user_max' }
 
@@ -38,23 +38,57 @@ describe 'continuous_queries', integration: true do
         ])
       end
     end
+
+    context 'when there are some continuous queries', version: '>0.8.3' do
+      let(:continuous_query1){ 'select MEAN(user) as user_mean from cpu group by time(1m) into cpu.1m.user_mean' }
+      let(:continuous_query2){ 'select MAX(user) as user_max from cpu group by time(1m) into cpu.1m.user_max' }
+
+      before do
+        subject.create(continuous_query1)
+        subject.create(continuous_query2)
+      end
+
+      it 'returns the list of users' do
+        expect(subject.all).to match_array([
+          { 'time' => 0, 'id' => 1, 'query' => continuous_query1 },
+          { 'time' => 0, 'id' => 2, 'query' => continuous_query2 }
+        ])
+      end
+    end
   end
 
   describe '.create' do
     let(:continuous_query){ 'select MEAN(user) as user_mean from cpu group by time(1m) into cpu.1m.user_mean' }
 
-    it 'creates a new query' do
+    it 'creates a new query', version: '<=0.8.3' do
       subject.create(continuous_query)
       expect(subject.all).to eq([{ 'id' => 1, 'query' => continuous_query }])
     end
 
-    context 'when there is the same query' do
+    it 'creates a new query', version: '>0.8.3' do
+      subject.create(continuous_query)
+      expect(subject.all).to eq([{ 'time' => 0, 'id' => 1, 'query' => continuous_query }])
+    end
+
+    context 'when there is the same query', version: '<=0.8.3' do
       before{ subject.create(continuous_query) }
 
       it 'creates one more' do
         subject.create(continuous_query)
         expect(subject.all).to match_array([
           { 'id' => 1, 'query' => continuous_query }, { 'id' => 2, 'query' => continuous_query }
+        ])
+      end
+    end
+
+    context 'when there is the same query', version: '>0.8.3' do
+      before{ subject.create(continuous_query) }
+
+      it 'creates one more' do
+        subject.create(continuous_query)
+        expect(subject.all).to match_array([
+          { 'time' => 0, 'id' => 1, 'query' => continuous_query },
+          { 'time' => 0, 'id' => 2, 'query' => continuous_query }
         ])
       end
     end
@@ -74,6 +108,7 @@ describe 'continuous_queries', integration: true do
 
       it 'removes db and returns true' do
         expect(subject.delete(1)).to be_truthy
+        expect(subject.all).to eq([])
       end
     end
   end
